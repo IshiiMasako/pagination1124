f4ebf31611b39a74588c54f7f4b3ca9358f7ae9f
IshiiMasako <C200020@ccg.ac.jp>
2021-11-24 13:32:44 +0900

    update 11/24

--- list.php
+++ list.php
@@ -1,5 +1,6 @@
 ﻿<?php
 require_once("functions.php");
+/* ■　1.POSTメソッドに加え、GETメソッドで遷移してくる場合を追加
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     if(isset($_POST["name"])){
         if(!empty($_POST["name"])) {
@@ -7,10 +8,41 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
         }
     }
 }
+*/
+define('MAXITEM',5);    // 最大表示件数
+	
+if($_SERVER['REQUEST_METHOD'] === 'POST'){     // 最初の条件検索時
+    if(isset($_POST["name"])){
+        $name = htmlspecialchars($_POST["name"], ENT_QUOTES, 'UTF-8');
+    }
+    $page = 1;   // 初期表示は1ページ
+} elseif($_SERVER['REQUEST_METHOD'] === 'GET'){  // ページネーション時
+    if (isset($_GET['page'])) {
+        $page = $_GET['page'];    //[ページ番号を設定処理1] 
+        $name = htmlspecialchars($_GET["name"], ENT_QUOTES, 'UTF-8');
+    } else {
+        $page = 1;    //[ページ番号を設定処理2] 
+        $name = htmlspecialchars($_GET["name"], ENT_QUOTES, 'UTF-8');
+    }
+
+    // スタートのポジションを計算する
+    // 取得するレコードの先頭位置を求める
+    if ($page > 1) {
+           // 例：２ページ目の場合は、『(2ページ目 × 最大表示件数) - 最大表示件数 = 5』
+        $start = $page * MAXITEM - MAXITEM; //[レコードの先頭位置を計算]   // $start変数に設定
+    } else {
+        $start = 0;  //[レコードの先頭位置を設定]   // 1ページ目の場合は先頭 0
+    }
+}
+//■　1.ここまで
+
+
+
 
 $dbh = db_conn();
 $data = [];
 
+/* ●　2.データ取得のSQL文を変更
 try{
     $sql = "SELECT * FROM user WHERE name like :name";
     $stmt = $dbh->prepare($sql);
@@ -21,6 +53,23 @@ try{
         $data[] = $row;
         $count++;
     }
+    */
+
+try{
+    $sql = "SELECT * FROM user WHERE name like :name LIMIT :page , :start /*[SQL文に追加]*/";
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindValue(':name', '%'.$name.'%', PDO::PARAM_STR);
+    $stmt->bindValue(':start', $start, PDO::PARAM_INT);
+    $stmt->bindValue(':page', MAXITEM, PDO::PARAM_INT);
+    $stmt->execute();
+    $count = 0;
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        $data[] = $row;
+        $count++;
+}
+// ●　2.ここまで
+
+
 
 }catch (PDOException $e){
     echo($e->getMessage());
@@ -68,11 +117,45 @@ try{
     <?php endforeach; ?>
 </table>
 <p style="margin:8px;">
+
+<!--// ▲　3.ページの下にページ送りのリンクを表示させる
 <form action="" method="POST">
 <div class="button-wrapper">
     <button type="button" onclick="history.back()">戻る</button>
 </div>
+</form>  -->
+
+<form action="" method="GET">
+	
+	<div>
+	    <p>現在 <?php echo $page; ?> ページ目です。</p>
+	<?php
+	   $stmt = $dbh->prepare("SELECT COUNT(*) id FROM user WHERE name like :name");
+	   $stmt->bindValue(':name', '%'.$name.'%', PDO::PARAM_STR);
+	   $stmt->execute();
+	   $page_num = $stmt->fetchColumn();
+	   // ページネーションの数を取得する
+	   $pagination = ceil($page_num / MAXITEM);  //[総ページ数の計算処理]
+	?>
+	<?php 
+	   for ($x=1; $x <= $pagination ; $x++) {
+	      if( $page == $x  /*[表示ページの場合]*/){
+		      echo $x;
+	      } else {
+	          echo ' ';
+	          echo '<a href=?page='. $x. '&name='. $name.'>'. $x. '</a>';
+		      echo ' ';
+		  }
+	   }
+	?>
+	</div>
+	
+	<div class="button-wrapper">
+	    <button type="button" onclick="history.back()">戻る</button>
+	</div>
 </form>
+<!--// ▲　3.ここまで-->
+
 <hr>
 <div class="container">
     <footer>
